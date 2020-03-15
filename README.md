4c2b564970822dce1c4c4aba68d8ecc7cdcb7491
d33pblue <francescob994@protonmail.com>
2020-03-15 22:20:28 +0100

    publish converter

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # unity-raw-exporter
-Simple image exporter for unity's heightmap
+This is a simple heightmap exporter for Unity. It takes a PNG image and converts it in a raw file Unity can read.
+
+## How to build
+First you need to download or clone this repository. Then:
+- *for linux* you can build with g++:
+```
+g++ -o heightmapExporter heightmapExporter.cpp
+```
+- *for windows* you can use Visual Studio or MinGW
+
+## How to use
+You can simply run in your shell:
+```
+./heightmapExporter "path/to/imageToConvert.png"
+```
+and the heightmap is stored in "terr.raw" file.
+
+## Dependencies
+The picopng library is used in order to load the PNG images. You can find this library at https://lodev.org/lodepng/picopng.cpp
---
+++ heightmapExporter.cpp
@@ -0,0 +1,36 @@
+// Copyright (c) 2020 d33pblue
+/* This program can convert a PNG image into a raw heightmap for Unity.
+* It considers only the r channel in the png (0-255) as height.
+* The library picopng is used in order to load the PNG image.
+* see: https://lodev.org/lodepng/picopng.cpp
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "dependencies/picopng.h"
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+  string path = argv[1];
+  cout<<"Convering "<<path<<endl;
+  std::vector<unsigned char> buffer,out_image;
+  loadFile(buffer, path);
+  unsigned long image_width,image_height;
+  if(decodePNG(out_image,image_width,image_height,
+      &buffer[0], (unsigned long)buffer.size())){
+    cout<<"Error!"<<endl;
+    return 1;
+  }
+  FILE* pFile = fopen ("terr.raw", "wb");
+  for(int i=0;i<image_width;i++){
+    for(int j=0;j<image_height;j++){
+      __int16_t d = (__int16_t)out_image[(i*image_height+j)*4];
+      d = d*100;
+      fwrite(&d,sizeof(__int16_t),1,pFile);
+    }
+  }
+  fclose(pFile);
+  cout<<"ok!"<<endl;
+  return 0;
+}
